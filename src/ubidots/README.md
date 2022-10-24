47e3e8ef8802a6a55012768c723b359ada7a6983
ge0053 <georg.hofstad@gmail.com>
2022-10-24 17:31:04 +0200

    moved som library files and addet Ubidots library

--- src/ubidots/README.md
+++
@@ -1,2 +0,0 @@
-i "secrets.h" må  UBIDOTS_TOKEN, WIFI_SSID og  WIFI_PASS settes til riktige verdier.
-esp32-mqtt-main.zip er biblioteket som er brukt for ubidots.
--- src/ubidots/secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef secrets_h
-#define secrets_h
-
-const char *UBIDOTS_TOKEN = "";  // Put here your Ubidots TOKEN
-const char *WIFI_SSID = "";      // Put here your Wi-Fi SSID
-const char *WIFI_PASS = "";      // Put here your Wi-Fi password
-
-
-#endif
