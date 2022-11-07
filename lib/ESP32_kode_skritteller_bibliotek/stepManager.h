dcda1bd8b344f6eee10b03ada6d92bd84010590f
ge0053 <georg.hofstad@gmail.com>
2022-11-07 15:15:47 +0100

    added comments to step manager.h

--- lib/ESP32_kode_skritteller_bibliotek/stepManager.h
+++ lib/ESP32_kode_skritteller_bibliotek/stepManager.h
@@ -7,17 +7,17 @@
 
 class stepManager {
   public:
-    void init ();
-    void registerStep();
-    unsigned int getCurrentCycleSteps();
-    unsigned long   getCurrentDaySteps();
-    void resetStepCycle();
+    void init ();                          //initialize
+    void registerStep();                   //register one step
+    unsigned int getCurrentCycleSteps();   // get the current amount og step.
+    unsigned long   getCurrentDaySteps();  //get total steps today
+    void resetStepCycle();                 //reset counter in current cycle. use after sending to Ubidots 
 
   private:
-    int getDateInYear();
+    int getDateInYear();                   //get the day in the curren year. 0 indexed.
 
-    unsigned int _currentCycleSteps;
-    unsigned long _currentDaySteps;
+    unsigned int _currentCycleSteps;       // number of steps in the current cycle
+    unsigned long _currentDaySteps;        //number of steps today.
     int _prevDayCycle;
 
 };
