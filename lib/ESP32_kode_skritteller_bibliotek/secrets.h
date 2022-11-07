8de64ec251f5c23090cb6c960fd4858bcea2f2b4
ge0053 <georg.hofstad@gmail.com>
2022-11-07 12:31:17 +0100

    g

--- lib/ESP32_kode_skritteller_bibliotek/secrets.h
+++ lib/ESP32_kode_skritteller_bibliotek/secrets.h
@@ -3,7 +3,7 @@
 
 const char *UBIDOTS_TOKEN = "";  // Put here your Ubidots TOKEN
 const char *WIFI_SSID = "";      // Put here your Wi-Fi SSID
-const char *WIFI_PASS = "";      // Put here your Wi-Fi password
+const char *WIFI_PASS = "1";      // Put here your Wi-Fi password
 
 
 #endif
