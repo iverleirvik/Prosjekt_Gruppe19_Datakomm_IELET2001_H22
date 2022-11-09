56718ad7e57662e207bd695d978858d6574fcaf3
erlebors <94974987+erlebors@users.noreply.github.com>
2022-11-09 13:49:26 +0100

    Litt rydding

--- lib/UbiSendReceive/UbiSendReceive.cpp
+++ lib/UbiSendReceive/UbiSendReceive.cpp
@@ -4,45 +4,44 @@ void UbiSendReceive::UbiSendReceive_CALLBACK(char *topic, byte *payload, unsigne
   char charWord[20];
   for (int i = 0; i < length; i++)
   {
-    //Serial.print((char)payload[i]);
     charWord[i] = (char)payload[i];
-  }// word = firstLetter + secondLetter
+  }
 
   Serial.println();
 
   if(strstr(topic, "/v2.0/devices/esp32/1/lv")){ // OBS: Pass på og endre topic-streng til ubidotsbrukeren!
-    skrittFelles.push_back(atoi(charWord));
-    }
+    skrittFelles.push_back(atoi(charWord));   // Legger verdi til i vectoren
+  }
 
   if(strstr(topic, "/v2.0/devices/esp32/2/lv")){
     newDay = true;
-    }
+  }
 }
 
-  void UbiSendReceive::UbiSendReceive_INIT(const char *VARIABLE_DATA, const char *VARIABLE_NEWDAY){
+void UbiSendReceive::UbiSendReceive_INIT(const char *VARIABLE_DATA, const char *VARIABLE_NEWDAY){
   ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_DATA);
   ubidots.subscribeLastValue(DEVICE_LABEL, VARIABLE_NEWDAY);
-  }
+}
 
- void UbiSendReceive::UbiSendReceive() {
+void UbiSendReceive::UbiSendReceive() {
   if (newDay == true){
     if (millis() > 1000 && notPubYet == false) {
-    Serial.println("pub");
-    ubidots.add(VARIABLE_LABEL, /*Skritt*/5);
-    ubidots.publish(DEVICE_LABEL);
-    notPubYet = true;
+      Serial.println("pub");
+      bidots.add(VARIABLE_LABEL, /*Skritt*/5);
+      ubidots.publish(DEVICE_LABEL);
+      notPubYet = true;
 
-    startTime = millis();
+      startTime = millis();
     }
 
     if(millis() > startTime + 9000) {
-    newDay = false;
-    notPubYet = false;
-    Serial.println("starter på nytt");
+      newDay = false;
+      notPubYet = false;
+      Serial.println("starter på nytt");
 
-    // LEGG INN ALVAR OG TORSTEIN FUNKSJON
+      // LEGG INN ALVAR OG TORSTEIN FUNKSJON
 
-    skrittFelles.clear();
+      skrittFelles.clear();
     }
   }
- }
+}
